730286693c9dbdad8bfff8cb19f3ee8ac8ac114b
irfanhussain <irfanmpcs@gmail.com>
2022-10-18 09:30:37 +0530

    third commit

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def homepage():
-    return "<h1>Hello this is python3 flask sample app </h1>"
+    return "<h1>Hello this is python3 flask sample app good service </h1>"
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=8080)
